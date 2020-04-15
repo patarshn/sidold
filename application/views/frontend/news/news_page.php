165478b403095bf7a017e05715e1ee74c15e204d
Yosua Sandi Susanto <38531813+yosuasandi1064@users.noreply.github.com>
2020-04-15 09:48:10 +0700

    Add files via upload

---
+++ application/views/frontend/news/news_page.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>BANK</title>
+</head>
+<body>
+<!-- NAVBAR -->
+<header id="header">
+    <nav class="navbar navbar-expand-lg navbar-light mybg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">LOGO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link" href="index.php">Home</a>
+                    <a class="nav-item nav-link" href="about.php">About Us</a>
+                    <a class="nav-item nav-link" href="product.php">Product</a>
+                    <a class="nav-item nav-link active" href="news.php">News</a>
+                    <a class="nav-item nav-link" href="contact.php">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+<!-- END NAVBAR -->
+<!-- JUMBOTRON -->
+<div class="jumbotron jumbotron-fluid about-image">
+    <div class="jumbotron-overlay d-flex">
+        <div class="container">
+          <h1 class="display-4 font-weight-bold text-white">News</h1>
+        </div>
+    </div>
+</div>
+<!-- END JUMBOTRON -->
+<div class="container">
+    <div class="row mt-4">
+        <div class="col-lg-9 teks">
+            <h3>Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis. Iaculis urna id volutpat lacus laoreet. Mauris vitae ultricies leo integer malesuada. Ac odio tempor orci dapibus ultrices in. Egestas diam in arcu cursus euismod. Dictum fusce ut placerat orci nulla. Tincidunt ornare massa eget egestas purus viverra accumsan in nisl. Tempor id eu nisl nunc mi ipsum faucibus. Fusce id velit ut tortor pretium. Massa ultricies mi quis hendrerit dolor magna eget. Nullam eget felis eget nunc lobortis. Faucibus ornare suspendisse sed nisi. Sagittis eu volutpat odio facilisis mauris sit amet massa. Erat velit scelerisque in dictum non consectetur a erat. Amet nulla facilisi morbi tempus iaculis urna. Egestas purus viverra accumsan in nisl. Feugiat in ante metus dictum at tempor commodo. Convallis tellus id interdum velit laoreet. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Dui faucibus in ornare quam. In iaculis nunc sed augue lacus viverra vitae congue. Vitae tempus quam pellentesque nec nam aliquam sem et. Ut morbi tincidunt augue interdum. Sem fringilla ut morbi tincidunt augue. Morbi enim nunc faucibus a pellentesque sit amet porttitor eget. In est ante in nibh mauris. Nam aliquam sem et tortor consequat id porta. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Dui faucibus in ornare quam. In iaculis nunc sed augue lacus viverra vitae congue. Vitae tempus quam pellentesque nec nam aliquam sem et. Ut morbi tincidunt augue interdum. Sem fringilla ut morbi tincidunt augue. Morbi enim nunc faucibus a pellentesque sit amet porttitor eget. In est ante in nibh mauris. Nam aliquam sem et tortor consequat id porta.</p>
+        </div>
+        <div class="col-lg">
+            <h5>NEWS</h5>
+            <div class="line"></div>
+            <div class="row mt-3">
+                <div class="col berita">
+                    <img src="img/news/1.jpg" alt="News 1" class="float-left">
+                    <h6 class="mt-2">Title</h6>
+                    <span>Kamis, 6 Februari 2020</span>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col berita">
+                    <img src="img/news/2.jpg" alt="News 2" class="float-left">
+                    <h6 class="mt-2">Title</h6>
+                    <span>Kamis, 6 Februari 2020</span>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col berita">
+                    <img src="img/news/3.jpg" alt="News 3" class="float-left">
+                    <h6 class="mt-2">Title</h6>
+                    <span>Kamis, 6 Februari 2020</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer id="footer">
+    <div class="container mt-4">
+        <div class="row content">
+            <div class="col-lg">
+                <h5>LOGO</h5>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae molestias laboriosam, ex quia aliquam nemo itaque nostrum expedita, minus corporis atque iste praesentium voluptatem? Ullam dignissimos in dicta excepturi sequi?</p>
+                <div class="social-media">
+                    <img src="img/icon/Icon simple-facebook.png" alt="Facebook">
+                    <img src="img/icon/Icon awesome-instagram.png" alt="Instagram">
+                    <img src="img/icon/Icon awesome-twitter.png" alt="Twitter">
+                </div>
+            </div>
+            <div class="col-lg">
+                <h5>Sitemap</h5>
+                <a href="about.php">About Us</a>
+                <a href="product.php">Product</a>
+                <a href="news.php">News</a>
+                <a href="contact.php">Contact</a>
+            </div>
+            <div class="col-lg">
+                <h5>Contact</h5>
+                <div class="row contact">
+                    <div class="col-1 icon">
+                        <img src="img/icon/Icon material-place.png" alt="Place">
+                    </div>
+                    <div class="col">
+                        <span>Jl. Way Pangubuan No. 16, Pahoman, Kota Bandarlampung, Lampung, 35132</span>
+                    </div>
+                </div>
+                <div class="row contact">
+                    <div class="col-1 icon">
+                        <img src="img/icon/Icon zocial-email.png" alt="Email">
+                    </div>
+                    <div class="col">
+                        <span>company@gmail.com</span>
+                    </div>
+                </div>
+                <div class="row contact">
+                    <div class="col-1 icon">
+                        <img src="img/icon/Icon ionic-ios-call.png" alt="Call">
+                    </div>
+                    <div class="col">
+                        <span>0812-3456-7890</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyrights justify-content-center d-flex">
+        <div class="col-lg-6">
+            <p>Copyrights © 2020, Company Name. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+<!-- END FOOTER -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
