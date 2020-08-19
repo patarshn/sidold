6bf3620959363a64df72ac7ac0e154c931268314
Patar Martua Doli Siahaan <malingno285@gmail.com>
2020-08-20 06:34:07 +0700

    Update index.php

--- application/views/frontend/service/index.php
+++ application/views/frontend/service/index.php
@@ -108,21 +108,21 @@
             </a>
             </div>
             <div class="col-3-lg mb-4">
-            <a href="form_belummenikah">
+            <a href="form_sudahmenikah">
                 <div class="card" style="border:0; box-shadow:6px 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                    <img src="<?=base_url('assets/my/img/16.png')?>" class="card-img-top" alt="product 11">
+                    <img src="<?=base_url('assets/my/img/16.png')?>" class="card-img-top" alt="product 12">
                     <div class="card-body">
-                        <h6>Form Belum Menikah</h6>
+                        <h6>Form Sudah Menikah</h6>
                     </div>
                 </div>
             </a>
             </div>
             <div class="col-3-lg mb-4">
-            <a href="form_sudahmenikah">
+            <a href="form_belummenikah">
                 <div class="card" style="border:0; box-shadow:6px 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                    <img src="<?=base_url('assets/my/img/17.png')?>" class="card-img-top" alt="product 12">
+                    <img src="<?=base_url('assets/my/img/17.png')?>" class="card-img-top" alt="product 11">
                     <div class="card-body">
-                        <h6>Form Sudah Menikah</h6>
+                        <h6>Form Belum Menikah</h6>
                     </div>
                 </div>
             </a>
